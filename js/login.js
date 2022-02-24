6d03090e44b1fe22430c962847447c1aa5793c14
Mauro Sanchez Matamoros <maurosanchezmatamoros@gmail.com>
2022-02-24 01:20:28 -0300

    Genera varios recibos con errores

--- js/login.js
+++ js/login.js
@@ -17,17 +17,19 @@ function signUp() {
     const newUser = document.getElementById("username-sign").value
     const newUserPass = document.getElementById("password-sign").value
     const userFound = Usuarios.find(user => user.username == newUser)
-    if(userFound){alert("El usuario ya se encuentra registrado")}
-    if(newUser.lenght < 6 || newUser.length > 12 || newUserPass.length < 6 || newUserPass.length > 12 ){
-        alert("El usuario y clave deben tener entre 6 y 12 caracteres")}
+    if(userFound){swal("El usuario ya se encuentra registrado", "", "warning")}
+    if(newUser.length < 6 || newUser.length > 12 || newUserPass.length < 6 || newUserPass.length > 12 ){
+        swal("El usuario y clave deben tener entre 6 y 12 caracteres", "", "warning")}
     else{
-    const newSignUp = new Login(newUser, newUserPass)
-    Usuarios.push(newSignUp)
-    console.log(Usuarios)
-    const UsuariosJSON = JSON.stringify(Usuarios)
-    localStorage.setItem("logins", UsuariosJSON)
-    createUser.addEventListener("click", window.location="../pages/liquidador.html")
-    }
+    swal("Usuario creado!", "", "success")
+    .then(() => {
+        const newSignUp = new Login(newUser, newUserPass)
+        Usuarios.push(newSignUp)
+        console.log(Usuarios)
+        const UsuariosJSON = JSON.stringify(Usuarios)
+        localStorage.setItem("logins", UsuariosJSON)
+        createUser.addEventListener("click", window.location="../pages/liquidador.html")
+    })}
 }
 
 function logIn() {
@@ -35,11 +37,13 @@ function logIn() {
     const newPassLog = document.getElementById("password").value
     const userFoundLog = Usuarios.findIndex(user => user.username == newUserLog)
     const userNotFound = !Usuarios.find(user => user.username == newUserLog)
-    if(userNotFound){alert("El usuario no se encuentra registrado")}
-    else if(Usuarios[userFoundLog].password!=newPassLog){alert("Contraseña incorrecta")}
+    if(userNotFound){swal("El usuario no se encuentra registrado", "", "warning")}
+    else if(Usuarios[userFoundLog].password!=newPassLog){swal("Contraseña incorrecta", "", "warning")}
     else{
+    swal("Has ingresado correctamente!", "", "success")
+    .then(() => {
     createUser.addEventListener("click", window.location="../pages/liquidador.html")
-    }
+    })}
 }
 
 const logUser = document.querySelector(".inicio__login__submit")
--- js/script.js
+++ js/script.js
@@ -74,6 +74,11 @@ enviarFormDatosGenerales.addEventListener("click", guardarForm1)
 
 function guardarForm1() {
 
+    let sub3 = document.querySelector(".sub3")
+    sub3.removeAttribute("style", "display")
+    let form2 = document.querySelector(".form2")
+    form2.setAttribute("style", "display: flex")
+
     let nombreEmpresa = document.getElementById("nombreEmpresa").value
     let cuitEmpresa = document.getElementById("cuitEmpresa").value
     let domicilioEmpresa = document.getElementById("domicilioEmpresa").value
@@ -90,10 +95,16 @@ enviarFormDatosEmpleados.addEventListener("click", guardarForm2)
 let empleadosCargadosUl = document.querySelector(".empleadosCargados")
 
 function guardarForm2() {
+
+    let sub4 = document.querySelector(".sub4")
+    sub4.removeAttribute("style", "display")
+    let form3 = document.querySelector(".form3")
+    form3.setAttribute("style", "display: flex")
+
     let legajo = parseInt(document.getElementById("legajo").value)
     let apellidoNombre = document.getElementById("apellidoNombre").value
     let cuil = document.getElementById("cuil").value
-    let sueldoHabitual = parseInt(document.getElementById("sueldoHabitual").value)
+    let sueldoHabitual = Number(document.getElementById("sueldoHabitual").value)
 
     const nuevoEmpleado = new claseEmpleados (apellidoNombre, legajo, cuil, sueldoHabitual)
     Empleados.push(nuevoEmpleado)
@@ -129,12 +140,14 @@ function guardarForm3() {
 
     for (i = 0 ; i < Empleados.length ; i++ ) {
 
+    let sub5 = document.querySelector(".sub5")
+    sub5.removeAttribute("style", "display")
     let titulo2 = document.querySelector(".titulo2")
-    titulo2.setAttribute("style", "visibility:visible")
+    titulo2.removeAttribute("style", "display")
     let tablaReciboSueldo = document.querySelector(".tablaReciboSueldo")
-    tablaReciboSueldo.setAttribute("style", "visibility:visible")
+    tablaReciboSueldo.setAttribute("style", "display: flex")
     let totales = document.querySelector(".totales")
-    totales.setAttribute("style", "visibility:visible")
+    totales.setAttribute("style", "display: flex")
 
     let anoLiquidacion = parseInt(document.getElementById("anoLiquidacion").value)
     let mesLiquidacion = parseInt(document.getElementById("mesLiquidacion").value)
@@ -142,7 +155,7 @@ function guardarForm3() {
 
     let sueldoHabitual = Empleados[i].sueldoHabitual
     let diasAusencias = parseInt(document.getElementById("diasAusencias").value)
-    let alicuotaSindicato = parseInt(document.getElementById("alicuotaSindicato").value)
+    let alicuotaSindicato = Number(document.getElementById("alicuotaSindicato").value)
     let ausencias = descontarAusencias(Empleados[i].sueldoHabitual, diasMesLiquidacion, diasAusencias)
     let sindicato = descontarSindicato(Empleados[i].sueldoHabitual, alicuotaSindicato)
 
@@ -155,31 +168,131 @@ function guardarForm3() {
 
     const nuevaLiquidacion = new claseLiquidaciones (anoLiquidacion, mesLiquidacion, diasMesLiquidacion, diasAusencias, ausencias, jubilacion, ley19032, ley23660, sindicato, sueldoHabitual)
     Liquidaciones.push(nuevaLiquidacion)
-
     }
-
-    console.log(Liquidaciones)
-
-    sumarTotales()
-    mostrarTotales()
 
 ////////////// COMPLETAR RECIBOS ////////////////////
 
-document.getElementById("nombreEmpresaRecibo").innerText = empleador.razonSocial
-document.getElementById("cuitEmpresaRecibo").innerText = empleador.cuitEmpresa
-document.getElementById("domicilioEmpresaRecibo").innerText = empleador.domicilioEmpresa
-document.getElementById("periodoLiquidacionRecibo").innerText = Liquidaciones[0].mesLiquidacion + "/" + Liquidaciones[0].anoLiquidacion
-document.getElementById("apellidoNombreRecibo").innerText = Empleados[0].apellidoNombre
-document.getElementById("cuilRecibo").innerText = Empleados[0].cuil
-document.getElementById("legajoRecibo").innerText = Empleados[0].legajo
-document.getElementById("sueldoHabitualRecibo").innerText = `$${Empleados[0].sueldoHabitual}`
-document.getElementById("sueldoBasicoRecibo").innerText = `$${Empleados[0].sueldoHabitual}`
-document.getElementById("ausenciasRecibo").innerText = `$${Liquidaciones[0].ausencias.toFixed(2)}`
-document.getElementById("jubilacionRecibo").innerText = `$${Liquidaciones[0].jubilacion.toFixed(2)}`
-document.getElementById("ley19032Recibo").innerText = `$${Liquidaciones[0].ley19032.toFixed(2)}`
-document.getElementById("ley23660Recibo").innerText = `$${Liquidaciones[0].ley23660.toFixed(2)}`
-document.getElementById("sindicatoRecibo").innerText = `$${Liquidaciones[0].sindicato.toFixed(2)}`
-document.getElementById("subtotalRemunerativoRecibo").innerText = `$${Liquidaciones[0].sueldoBruto.toFixed(2)}`
-document.getElementById("subtotalDescuentosRecibo").innerText = `$${Liquidaciones[0].descuentos.toFixed(2)}`
-document.getElementById("netoPagarRecibo").innerText = `$${Liquidaciones[0].sueldoNeto.toFixed(2)}`
+for (i = 0 ; i < Liquidaciones.length ; i++ ) {
+
+    let reciboSueldoNuevo = 
+`<table class="tablaReciboSueldo__in" cellspacing="0" cellpadding="3px">
+<tr>
+    <th colspan="4">Datos de la empresa</th>
+</tr>
+<tr>
+    <td>Razón social:</td>
+    <td colspan="3" id="nombreEmpresaRecibo"></td>
+</tr>
+<tr>
+    <td>CUIT:</td>
+    <td colspan="3" id="cuitEmpresaRecibo"></td>
+</tr>
+<tr>
+    <td>Domicilio:</td>
+    <td colspan="3" id="domicilioEmpresaRecibo"></td>
+</tr>
+<tr>
+    <td>Período de liquidación:</td>
+    <td colspan="3" id="periodoLiquidacionRecibo${i}"></td>
+</tr>
+<tr>
+    <th colspan="4">Datos del empleado</th>
+</tr>
+<tr>
+    <td>Apellido y Nombre:</td>
+    <td colspan="3" id="apellidoNombreRecibo${i}"></td>
+</tr>
+<tr>
+    <td>CUIL:</td>
+    <td colspan="3" id="cuilRecibo${i}"></td>
+</tr>
+<tr>
+    <td>Legajo N°:</td>
+    <td colspan="3" id="legajoRecibo${i}"></td>
+</tr>
+<tr>
+    <td>Sueldo categoría:</td>
+    <td colspan="3" id="sueldoHabitualRecibo${i}"></td>
+</tr>
+<tr>
+    <th>Concepto</th>
+    <th>Remunerativo</th>
+    <th>No remunerativo</th>
+    <th>Descuentos</th>
+</tr>
+<tr>
+    <td>Sueldo básico:</td>
+    <td id="sueldoBasicoRecibo${i}"></td>
+    <td></td>
+    <td></td>
+</tr>
+<tr>
+    <td>Ausencias:</td>
+    <td id="ausenciasRecibo${i}"></td>
+    <td></td>
+    <td></td>
+</tr>
+<tr>
+    <td>Jubilación:</td>
+    <td></td>
+    <td></td>
+    <td id="jubilacionRecibo${i}"></td>
+</tr>
+<tr>
+    <td>Ley 19032:</td>
+    <td></td>
+    <td></td>
+    <td id="ley19032Recibo${i}"></td>
+</tr>
+<tr>
+    <td>Ley 23660:</td>
+    <td></td>
+    <td></td>
+    <td id="ley23660Recibo${i}"></td>
+</tr>
+<tr>
+    <td>Sindicato:</td>
+    <td></td>
+    <td></td>
+    <td id="sindicatoRecibo${i}"></td>
+</tr>
+<tr>
+    <td>SUBTOTALES:</td>
+    <td id="subtotalRemunerativoRecibo${i}"></td>
+    <td id="subtotalNoRemunerativoRecibo${i}"></td>
+    <td id="subtotalDescuentosRecibo${i}"></td>
+</tr>
+<tr>
+    <td colspan="3">SUELDO NETO A PAGAR</td>
+    <td id="netoPagarRecibo${i}"></td>
+</tr>
+</table>`
+
+    let tablaReciboSueldo = document.querySelector(".tablaReciboSueldo")
+    tablaReciboSueldo.insertAdjacentHTML("beforeend", reciboSueldoNuevo)
+
+    document.getElementById("nombreEmpresaRecibo").innerText = empleador.razonSocial
+    document.getElementById("cuitEmpresaRecibo").innerText = empleador.cuitEmpresa
+    document.getElementById("domicilioEmpresaRecibo").innerText = empleador.domicilioEmpresa
+    document.getElementById(`periodoLiquidacionRecibo${i}`).innerText = Liquidaciones[i].mesLiquidacion + "/" + Liquidaciones[i].anoLiquidacion
+    document.getElementById(`apellidoNombreRecibo${i}`).innerText = Empleados[i].apellidoNombre
+    document.getElementById(`cuilRecibo${i}`).innerText = Empleados[i].cuil
+    document.getElementById(`legajoRecibo${i}`).innerText = Empleados[i].legajo
+    document.getElementById(`sueldoHabitualRecibo${i}`).innerText = `$${Empleados[i].sueldoHabitual}`
+    document.getElementById(`sueldoBasicoRecibo${i}`).innerText = `$${Empleados[i].sueldoHabitual}`
+    document.getElementById(`ausenciasRecibo${i}`).innerText = `$${Liquidaciones[i].ausencias.toFixed(2)}`
+    document.getElementById(`jubilacionRecibo${i}`).innerText = `$${Liquidaciones[i].jubilacion.toFixed(2)}`
+    document.getElementById(`ley19032Recibo${i}`).innerText = `$${Liquidaciones[i].ley19032.toFixed(2)}`
+    document.getElementById(`ley23660Recibo${i}`).innerText = `$${Liquidaciones[i].ley23660.toFixed(2)}`
+    document.getElementById(`sindicatoRecibo${i}`).innerText = `$${Liquidaciones[i].sindicato.toFixed(2)}`
+    document.getElementById(`subtotalRemunerativoRecibo${i}`).innerText = `$${Liquidaciones[i].sueldoBruto.toFixed(2)}`
+    document.getElementById(`subtotalDescuentosRecibo${i}`).innerText = `$${Liquidaciones[i].descuentos.toFixed(2)}`
+    document.getElementById(`netoPagarRecibo${i}`).innerText = `$${Liquidaciones[i].sueldoNeto.toFixed(2)}`
 }
+
+console.log(Liquidaciones)
+
+    sumarTotales()
+    mostrarTotales()
+}
+
